7294da7eb9976ffb3e98a30d82f5a638ba9f1765
KanyaT <kanyathyagaraj@gmail.com>
2024-05-18 12:14:25 +0530

    lesson-03

--- app/src/main/java/com/example/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/coderswag/Controller/MainActivity.kt
@@ -3,25 +3,26 @@ package com.example.coderswag.Controller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.ImageView
 import android.widget.ListView
-import com.example.coderswag.Model.Category
+import android.widget.TextView
+import com.example.coderswag.Adapter.CategoryAdapter
 import com.example.coderswag.R
 import com.example.coderswag.Services.DataService
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : ArrayAdapter<Category>
+    lateinit var adapter : CategoryAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this,
-        android.R.layout.simple_list_item_1,
-        DataService.categories)
+        adapter = CategoryAdapter(this,DataService.categories)
 
         val categoryListView = findViewById<ListView>(R.id.categoryListview)
 
         categoryListView.adapter = adapter
 
     }
+
 }
